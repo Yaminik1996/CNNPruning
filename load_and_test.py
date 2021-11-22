eb1e5a91ae1f32edfdf5f7e082e4fb965aa36054
Nidhi Chandra <chandranidhi45@gmail.com>
2021-11-21 20:34:47 -0500

    Modified load_and_test.py

--- load_and_test.py
+++ load_and_test.py
@@ -22,7 +22,7 @@ class Args:
   def __init__(self):
     self.batch_size = 256
     self.classifier = 'resnet18'
-    self.data_dir = '/data/huy/cifar10'
+    self.data_dir = 'data/huy/cifar10'
     self.dev = 0
     self.download_weights = 0
     self.gpu_id = '0'
@@ -39,38 +39,38 @@ if __name__ == "__main__":
   trainer = Trainer(
             fast_dev_run=bool(0),
             logger=logger if not bool(0 + 1) else None,
-            gpus=-1,
             deterministic=True,
             weights_summary=None,
             #log_every_n_steps=1,
             max_epochs=100,
             checkpoint_callback=None,
             precision=32,
+            accelerator="cpu"
         )
 
   args = Args()
   data = CIFAR10Data(args)
 
-  model1 = torch.load("one_shot_50.pth")
+  model1 = torch.load("one_shot_50.pth",map_location=torch.device('cpu'))
   print("Sparsity of model", calculate_sparsity(model1))
   trainer.test(model1, data.test_dataloader())
 
-  model2 = torch.load("one_shot_75.pth")
+  model2 = torch.load("one_shot_75.pth",map_location=torch.device('cpu'))
   print("Sparsity of model", calculate_sparsity(model2))
   trainer.test(model2, data.test_dataloader())
 
-  model3 = torch.load("one_shot_90.pth")
+  model3 = torch.load("one_shot_90.pth",map_location=torch.device('cpu'))
   print("Sparsity of model", calculate_sparsity(model3))
   trainer.test(model3, data.test_dataloader())
 
-  model4 = torch.load("iterative_50.pth")
+  model4 = torch.load("iterative_50.pth",map_location=torch.device('cpu'))
   print("Sparsity of model", calculate_sparsity(model4))
   trainer.test(model4, data.test_dataloader())
 
-  model5 = torch.load("iterative_75.pth")
+  model5 = torch.load("iterative_75.pth",map_location=torch.device('cpu'))
   print("Sparsity of model", calculate_sparsity(model5))
   trainer.test(model5, data.test_dataloader())
 
-  model6 = torch.load("iterative_90.pth")
+  model6 = torch.load("iterative_90.pth",map_location=torch.device('cpu'))
   print("Sparsity of model", calculate_sparsity(model6))
   trainer.test(model6, data.test_dataloader())
