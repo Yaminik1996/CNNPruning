f3c259a7a75fdeb5e3dffca97ca9f248f3e34c5f
Nidhi Chandra <chandranidhi45@gmail.com>
2021-11-21 22:58:40 -0500

    Modified running script

--- run.sh
+++ run.sh
@@ -4,3 +4,4 @@ gdown --id 1-ThHKHDiUMhGFcpbARRfhXNedgviz1vS
 gdown --id 1-UGCLmYHr8DpmpkW282Mc3iTbARH_lsY
 gdown --id 1-WfZPudvKrjQN9R9JJfm7wEo7xEzz3Dt
 gdown --id 1-_1D00a4g9FcLnTkshv2FtDDY9lEBQ2H
+python load_and_test.py
